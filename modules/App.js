3a840a821fef846e084130799cc33ba54e42f055
Paul Lazo <paullazo19@gmail.com>
2016-06-01 09:20:35 -0500

    added image toggle functionality and more styling

--- modules/App.js
+++ modules/App.js
@@ -34,73 +34,58 @@ export default React.createClass({
   },
   render() {
     return (
-      <section className="row">
-      {this.props.tshirts.map(function(tshirt, i){
-        return <div className="col-md-4" key={i}>
-          <div className="">
-            <div id="imageSwap" className="carousel" data-ride="carousel" data-interval="false">
-              <div className="carousel-inner">
-                <div className="item active">
-                  <img src={tshirt.imageUrlFront} className="view__tshirt--front"/>
-                </div>
-                <div className="item">
-                  <img src={tshirt.imageUrlBack} className="view__tshirt--back"/>
+      <section>
+        <div className="row">
+        {this.props.tshirts.map(function(tshirt, i){
+          return <div className="col-md-4" key={i}>
+            <div className="">
+              <div id="imageSwap" className="carousel" data-ride="carousel" data-interval="false">
+                <a className="view__tshirt--flip carousel-swap" href="#imageSwap" role="button" data-slide="prev">
+                  <i className="flip fa fa-exchange fa-2x" aria-hidden="true"></i>
+                </a>
+                <div className="carousel-inner">
+                  <div className="item active">
+                    <img src={tshirt.imageUrlFront} className="view__tshirt--front"/>
+                  </div>
+                  <div className="item">
+                    <img src={tshirt.imageUrlBack} className="view__tshirt--back"/>
+                  </div>
                 </div>
               </div>
-              <a className="view__tshirt--flip carousel-swap" href="#imageSwap" role="button" data-slide="prev">
-                <i className="fa fa-exchange" aria-hidden="true"></i>
-              </a>
+                    <ul className="view__nav">
+                      <li className="button view__nav--link">
+                        <a className="" href="#">
+                          <i className="fa fa-exchange fa-2x" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                      <li className="button view__nav--link">
+                        <a className="" href="#">
+                          <i className="fa fa-heart-o fa-2x" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                      <li className="button view__nav--link">
+                        <a className="shirtSize__icon--default" href="#">XL</a>
+                      </li>
+                      <li className="button view__nav--link">
+                        <div className="color__current color">X</div>
+                        <ul>
+                          <li className="color"></li>
+                          <li className="color"></li>
+                          <li className="color"></li>
+                          <li className="color"></li>
+                        </ul>
+                      </li>
+                      <li className="button view__nav--link">
+                        <a className="" href="#">
+                          <i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
+                        </a>
+                      </li>
+                    </ul>
+                    <p className="view__tshirt--name">{tshirt.title}<span className="view__tshirt--price">{`$${tshirt.price}`}</span></p>
             </div>
-                  <ul className="view__nav">
-                    <li className="button view__nav--link">
-                      <a className="" href="#">
-                        <i className="fa fa-exchange" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li className="button view__nav--link">
-                      <a className="" href="#">
-                        <i className="fa fa-heart-o" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                    <li className="button view__nav--link">
-                      <a className="" href="#">XL</a>
-                    </li>
-                    <li className="button view__nav--link">
-                      <div className="col or__current color"></div>
-                      <a className="" href="#"></a>
-                      <ul>
-                        <li className="color"></li>
-                        <li className="color"></li>
-                        <li className="color"></li>
-                        <li className="color"></li>
-                      </ul>
-                    </li>
-                    <li className="button view__nav--link">
-                      <a className="" href="#">
-                        <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-                      </a>
-                    </li>
-                  </ul>
-                  <p className="view__tshirt--name">{tshirt.title}<span className="view__tshirt--price">{tshirt.price}</span></p>
           </div>
+        })}
         </div>
-      })}
-
-      <div className="dropup">
-        <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropup
-          <span className="caret"></span>
-        </button>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li role="separator" className="divider"></li>
-          <li><a href="#">Separated link</a></li>
-        </ul>
-      </div>
-
-
       </section>
 
 
